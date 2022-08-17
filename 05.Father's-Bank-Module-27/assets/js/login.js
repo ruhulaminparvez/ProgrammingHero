6c446209e229e9dbefbdc31220c9e9238061d1a6
Ruhul Amin Parvez <ruhulaminparvez007@gmail.com>
2022-08-17 12:15:00 +0600

    Father's Bank Initialized

---
+++ 05.Father's-Bank-Module-27/assets/js/login.js
@@ -0,0 +1,13 @@
+document.getElementById('login').addEventListener('click', function() {
+    const userMail = document.getElementById('user-mail');
+    const userField = userMail.value;
+    
+    const userPass = document.getElementById('user-password');
+    const passField = userPass.value;
+
+    if (userField === 'ruhulamin@gmail.com' && passField === '623492'){
+        window.location.href = 'dashboard.html';
+    } else{
+        alert('Invalid User! Please, Enter Correct Password.')
+    }
+})
