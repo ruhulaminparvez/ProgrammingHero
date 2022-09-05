28585cf139dfee5237ad88dfffab81198eda236b
Ruhul Amin Parvez <ruhul.parvez@oisbd.net>
2022-09-05 16:49:47 +0600

    Phone Single Detail Done

--- 08.Phone-Hunter-APIs-Module-34/assets/js/api.js
+++ 08.Phone-Hunter-APIs-Module-34/assets/js/api.js
@@ -19,7 +19,7 @@ const displayPhones = (phones) => {
           <h5 class="card-title text-center">${phone.phone_name}</h5>
         </div>
         <div class="card-footer">
-          <button onclick="loadDetails('${phone.slug}')" class="btn btn-info text-white d-flex justify-content-center m-auto" data-bs-toggle="modal" data-bs-target="#phoneModal">View Details</button>
+          <button onclick="loadDetails('${phone.slug}')" type="button" class="btn btn-info text-white d-flex justify-content-center m-auto" data-bs-toggle="modal" data-bs-target="#exampleModal">View Details</button>
         </div>
       </div>
     `
@@ -37,72 +37,67 @@ const loadDetails = async (phoneId) => {
 
 const displayDetails = (singleDetail) => { 
   const phoneDetails = document.getElementById('get-phone-details');
-  
+  phoneDetails.textContent = '';
+
   const parentDiv = document.createElement('div');
   parentDiv.innerHTML = `
-        <div class="modal fade" id="phoneModal" tabindex="-1" aria-labelledby="phoneModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-          <div class="modal-header">
-            <h5 class="modal-title" id="phoneModalLabel">${singleDetail.name} - Details</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <div class="d-flex justify-content-center align-items-center">
-              <img class="img-fluid" src="${singleDetail.image}">
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="phoneModalLabel">${singleDetail.name} - Details</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <div class="d-flex justify-content-center align-items-center">
+                    <img class="img-fluid" src="${singleDetail.image}">
+                  </div>
+                  <div>
+                    <table class="table table-striped table-hover my-5">
+                      <tbody>
+                        <tr>
+                          <th scope="row">Name</th>
+                          <td colspan="6">${singleDetail.name}</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">Brand</th>
+                          <td colspan="6">${singleDetail.brand}</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">ReleaseDate</th>
+                          <td colspan="6">${singleDetail.releaseDate}</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">Storage</th>
+                          <td colspan="6">${singleDetail.mainFeatures.storage}</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">Display</th>
+                          <td colspan="6">${singleDetail.mainFeatures.displaySize}</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">ChipSet</th>
+                          <td colspan="6">${singleDetail.mainFeatures.chipSet}</td>
+                        </tr>
+                        <tr>
+                          <th scope="row">Memory</th>
+                          <td colspan="6">${singleDetail.mainFeatures.memory}</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+              </div>
             </div>
-            <div>
-              <table class="table table-striped table-hover my-5">
-                <tbody>
-                  <tr>
-                    <th scope="row">Name</th>
-                    <td colspan="6">${singleDetail.name}</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Brand</th>
-                    <td colspan="6">${singleDetail.brand}</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">ReleaseDate</th>
-                    <td colspan="6">${singleDetail.releaseDate}</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Storage</th>
-                    <td colspan="6">${singleDetail.mainFeatures.storage}</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Display</th>
-                    <td colspan="6">${singleDetail.mainFeatures.displaySize}</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">ChipSet</th>
-                    <td colspan="6">${singleDetail.mainFeatures.chipSet}</td>
-                  </tr>
-                  <tr>
-                    <th scope="row">Memory</th>
-                    <td colspan="6">${singleDetail.mainFeatures.memory}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
           </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          </div>
-        </div>
-      </div>
   `;
   phoneDetails.appendChild(parentDiv);
 
 }
-                 // <tr>
-                  //   <th scope="row">Sensors</th>
-                  //   <td>Face ID</td>
-                  //   <td>accelerometer</td>
-                  //   <td>gyro</td>
-                  //   <td>proximity</td>
-                  //   <td>compass</td>
-                  //   <td>barometer</td>
-                  // </tr>
 
 
 document.getElementById('btn-search').addEventListener('click', function () {
